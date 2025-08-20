d6d791fa39c4a157e530b86b2fe43e66402426e5
biplavpaudel123 <biplavpaudel123@gmail.com>
2025-08-20 13:23:43 +0545

    todos done with Redux TOolkit

--- reduxToolkit-Todo/src/App.jsx
+++ reduxToolkit-Todo/src/App.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import Todos from './components/Todos.jsx'
+import AddTodo from './components/AddTodo.jsx'
+import {Provider} from 'react-redux'
+import { store } from './app/store'
 
 function App() {
   return (
-    <div>App</div>
+    <Provider store={store}>
+    <div>Todos using Redux Toolkit</div>
+    <AddTodo/>
+    <Todos/>
+    </Provider>
   )
 }
 
